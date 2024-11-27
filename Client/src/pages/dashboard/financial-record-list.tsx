57359f5cfde7eeb714a8bd23c761f99329d15719
maverickOG <heysajit@gmail.com>
2024-11-27 17:05:46 +0530

    Added components to add entries in dashboard page

---
+++ Client/src/pages/dashboard/financial-record-list.tsx
@@ -0,0 +1,7 @@
+export const FinancialRecordList = () => {
+	return (
+		<div className="financial-record-list-container">
+			<h1>Record List</h1>
+		</div>
+	)
+}
--- Client/src/pages/dashboard/index.tsx
+++ Client/src/pages/dashboard/index.tsx
@@ -1,7 +1,15 @@
+import { useUser } from '@clerk/clerk-react'
+import { FinancialRecordForm } from './financial-record-form'
+import { FinancialRecordList } from './financial-record-list'
+
 export const Dashboard = () => {
+
+	const { user } = useUser()
 	return (
-		<div>
-			<h1>Dashboard</h1>
+		<div className="dashboard-container">
+			<h1>Welcome to Mindful About Money, {user?.firstName}! Here are your finances</h1>
+			<FinancialRecordForm />
+			<FinancialRecordList />
 		</div>
 	)
 }
