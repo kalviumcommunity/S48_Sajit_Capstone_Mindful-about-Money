7aba28519217ef7cdc89e395effd61887d231653
maverickOG <heysajit@gmail.com>
2024-12-18 00:05:32 +0530

    Added something that was unused

--- Client/src/pages/contact.tsx
+++ Client/src/pages/contact.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Twitter, Linkedin, Github, Mail, MapPin, Phone } from "lucide-react";
+import { Twitter, Linkedin, Github, Mail, MapPin } from "lucide-react";
 
 const ContactPage: React.FC = () => {
   const socialLinks = [
